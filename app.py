26275eb32a67c48a1f2a1024c7d5c3e57a28e7b2
Ameename18 <ameename1818@gmail.com>
2026-02-26 21:16:57 +0530

    Fixed syntax error

--- app.py
+++ app.py
@@ -18,10 +18,12 @@ def connect_google_sheets():
         "https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/drive"
     ]
-    ccreds = ServiceAccountCredentials.from_json_keyfile_dict(
-        st.secrets["gcp_service_account"], scope
+
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(
+        st.secrets["gcp_service_account"],
+        scope
     )
-    )
+
     client = gspread.authorize(creds)
     return client.open("The Good Future Project – Chitti Database")
 
